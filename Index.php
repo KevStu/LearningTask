3ba927347b59e7c5030dbdca5b2211091e00d3fd
KevStu <100114266+KevStu@users.noreply.github.com>
2022-02-21 14:19:58 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Page</title>
+</head>
+<body>
+<h1>Recipes</h1>
+<ul>
+<li><a href="Index.php">HOME</a></li>
+<li><a href="/LT2/Index.html">LEARNING TASK 2</a><li>
+</ul>
+</body>
+</html>
