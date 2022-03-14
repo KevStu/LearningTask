27f69bfb3e9052da46f69ef0b66541a0aa2e30c9
KevStu <100114266+KevStu@users.noreply.github.com>
2022-03-14 16:00:03 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,9 +4,9 @@
     <title>My Home Page</title>
   </head>
   <body>
-    <h1>My Recipes</h1>
+    <h1>Welcome to My Website</h1>
     <ul>
-      <li><a href="Index.html">HOME</a></li>
+      <li><a href="Index.php">HOME</a></li>
       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
     </ul>
   </body>
