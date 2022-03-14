813a0233a24301f74137537b7ea341ce7e94118d
KevStu <100114266+KevStu@users.noreply.github.com>
2022-03-14 18:24:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! I am Kevin De la Cruz</h1>
     <ul>
       <li><a href="Index.php">HOME</a></li>
-      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+      <li><a href="/7 Recipes/index.html">LEARNING TASK 2</a></li>
       <li><a href="/LT3/Index.html">LEARNING TASK 3</a></li>
     </ul>
   </body>
